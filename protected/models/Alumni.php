f546915d6b86fc5efc7e92f97fa512490317c691
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-21 18:16:49 +0700

    Add pagesize 20 in pagination

--- protected/models/Alumni.php
+++ protected/models/Alumni.php
@@ -133,6 +133,7 @@ class Alumni extends CActiveRecord
 
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
+                        'pagination'=>array('pageSize'=> 20),
 		));
 	}
 }
