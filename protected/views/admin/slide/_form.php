29e3ca7bf1595ab58fbbf6f2429a36e93d287a26
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-25 23:50:45 +0700

    Change Layout form

--- protected/views/admin/slide/_form.php
+++ protected/views/admin/slide/_form.php
@@ -18,44 +18,44 @@
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'title_en'); ?>
-		<?php echo $form->textField($model,'title_en',array('size'=>50,'maxlength'=>100)); ?> ความยาวไม่เกิน 100 ตัวอักษร
+		<?php echo $form->textField($model,'title_en',array('size'=>50,'maxlength'=>100)); ?>
 		<?php echo $form->error($model,'title_en'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'title_th'); ?>
-		<?php echo $form->textField($model,'title_th',array('size'=>50,'maxlength'=>100)); ?> ความยาวไม่เกิน 100 ตัวอักษร
+		<?php echo $form->textField($model,'title_th',array('size'=>50,'maxlength'=>100)); ?>
 		<?php echo $form->error($model,'title_th'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'desc_en'); ?>
-                <?php echo $form->textArea($model,'desc_en',array('rows'=>3, 'cols'=>50)); ?> ความยาวไม่เกิน 255 ตัวอักษร
+                <?php echo $form->textArea($model,'desc_en',array('rows'=>3, 'cols'=>50)); ?>
 		<?php echo $form->error($model,'desc_en'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'desc_th'); ?>
-		<?php echo $form->textArea($model,'desc_th',array('rows'=>3, 'cols'=>50)); ?> ความยาวไม่เกิน 255 ตัวอักษร
+		<?php echo $form->textArea($model,'desc_th',array('rows'=>3, 'cols'=>50)); ?>
 		<?php echo $form->error($model,'desc_th'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'link_en'); ?>
-		<?php echo $form->textField($model,'link_en',array('size'=>60,'maxlength'=>255)); ?> ตัวอย่าง http://www.gspa.buu.ac.th
+		<?php echo $form->textField($model,'link_en',array('size'=>60,'maxlength'=>255)); ?>
 		<?php echo $form->error($model,'link_en'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'link_th'); ?>
-		<?php echo $form->textField($model,'link_th',array('size'=>60,'maxlength'=>255)); ?> ตัวอย่าง http://www.gspa.buu.ac.th
+		<?php echo $form->textField($model,'link_th',array('size'=>60,'maxlength'=>255)); ?>
 		<?php echo $form->error($model,'link_th'); ?>
 	</div>
 
 	<div class="row">           
-		<?php echo $form->labelEx($model,'image'); ?>
+		<?php echo $form->labelEx($model,'image'); ?><br/>
 		<?php if(!$model->isNewRecord) echo CHtml::image(Yii::app()->request->baseUrl . '/uploads/slide/' . $model->image, '', array('style'=>'width: 120px')); ?><br />
-		<?php echo $form->fileField($model,'image',array('style'=>'border: none;box-shadow:none')); ?> ขนาดรูปภาพ กว้าง 830px ยาว 431px <br/>
+		<?php echo $form->fileField($model,'image',array('style'=>'border: none;box-shadow:none')); ?> <br/>ขนาดรูปภาพ กว้าง 830px ยาว 431px <br/>
                 (ไฟล์รูปภาพนามสกุล jpg, jpeg, gif, png เท่านั้น)
 		<?php echo $form->error($model,'image'); ?>
 	</div>
