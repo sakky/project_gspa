5f3f4ce52cc3807513013c9637b72ffbfa707a9f
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-28 00:18:39 +0700

    Add Cancel button

--- protected/views/admin/studentNews/_form.php
+++ protected/views/admin/studentNews/_form.php
@@ -83,7 +83,7 @@
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'status'); ?>
-		<?php echo $form->textField($model,'status'); ?>
+		<?php echo $form->dropDownList($model, 'status', array('1'=>'แสดง','0'=>'ไม่แสดง')); ?>
 		<?php echo $form->error($model,'status'); ?>
 	</div>
 
@@ -93,14 +93,9 @@
 		<?php echo $form->error($model,'user_id'); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'time_stamp'); ?>
-		<?php echo $form->textField($model,'time_stamp'); ?>
-		<?php echo $form->error($model,'time_stamp'); ?>
-	</div>
-
 	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'เพิ่มข้อมูล' : 'บันทึกข้อมูล'); ?>&nbsp;&nbsp;
+                <?php echo CHtml::Button('ยกเลิก', array('submit' => array('index'))); ?>
 	</div>
 
 <?php $this->endWidget(); ?>
