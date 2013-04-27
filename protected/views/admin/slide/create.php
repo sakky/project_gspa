1f59c15b61e7c47a1c4c3523df515059ec74ab71
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-28 01:31:44 +0700

    Change Text

--- protected/views/admin/slide/create.php
+++ protected/views/admin/slide/create.php
@@ -3,16 +3,16 @@
 /* @var $model Slide */
 
 $this->breadcrumbs=array(
-	'Slides'=>array('index'),
-	'Create',
+	'Slide'=>array('index'),
+	'เพิ่มข้อมูล',
 );
 
 $this->menu=array(
 	//array('label'=>'List Slide', 'url'=>array('index')),
-	array('label'=>'Manage Slide', 'url'=>array('admin')),
+	array('label'=>'จัดการ Slide', 'url'=>array('admin')),
 );
 ?>
 
-<h1>Create Slide</h1>
+<h1>เพิ่ม Slide</h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model,'order_list'=>$order_list)); ?>
