0beb4a9e5f6b7df325fd6a21c2d1b6b96b6f2b11
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-28 01:20:14 +0700

    Fix Width for Image

--- protected/views/admin/slide/admin.php
+++ protected/views/admin/slide/admin.php
@@ -4,12 +4,13 @@
 
 $this->breadcrumbs=array(
 	'Slides'=>array('index'),
-	'Manage',
+	'จัดการข้อมูล',
 );
 
 $this->menu=array(
 	//array('label'=>'List Slide', 'url'=>array('index')),
-	array('label'=>'Create Slide', 'url'=>array('create')),
+	array('label'=>'เพิ่ม Slide', 'url'=>array('create')),
+        array('label'=>'เรียงลำดับ Slide', 'url'=>array('order')),
 );
 
 Yii::app()->clientScript->registerScript('search', "
@@ -26,14 +27,9 @@ $('.search-form form').submit(function(){
 ");
 ?>
 
-<h1>Manage Slides</h1>
+<h1>จัดการ Slide</h1>
 
-<p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
-
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<?php echo CHtml::link('ค้นหาแบบละเอียด','#',array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
 <?php $this->renderPartial('_search',array(
 	'model'=>$model,
@@ -50,16 +46,25 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 			'header'=>'ID',
 			'htmlOptions'=>array('style'=>'text-align: center;width: 30px;'),
 		),
-		'title_en',
-		'title_th',
+                array(
+			'name'=>'image',
+			'htmlOptions'=>array('style'=>'text-align: center;width: 100px;'),
+                        'type'=>'html',
+                        'value'=>'CHtml::image(Yii::app()->request->baseUrl ."/uploads/slide/".$data->image, "รูป Slide",array(\'width\'=>120))',
+                ),
+            	array(
+			'name'=>'title_th',                        
+                        'header'=>'ชื่อเรื่อง',
+		),
                 array(
 			'name'=>'sort_order',
 			'htmlOptions'=>array('style'=>'text-align: center;width: 60px;'),
 		),
 		array(
 			'name'=>'status',                 
-			'value'=> '($data->status)? \'Enabled\' : \'Disabled\'',
+			'value'=> '($data->status)? \'แสดง\' : \'ไม่แสดง\'',
 			'htmlOptions'=>array('style'=>'text-align: center;width: 60px;'),
+                        'filter'=>array('1'=>'แสดง','0'=>'ไม่แสดง'),
 		),
                 /*array(
 			'name'=>'desc_en',
