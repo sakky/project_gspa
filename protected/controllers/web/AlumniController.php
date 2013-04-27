37bb2bad7953d60aaca9992e3a10bd93ad189226
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-27 20:07:55 +0700

    Add Alumni Controller

---
+++ protected/controllers/web/AlumniController.php
@@ -0,0 +1,49 @@
+<?php
+
+class AlumniController extends Controller
+{
+	public function actionIndex()
+	{
+                if(isset($_GET['id'])){
+                    $model=Alumni::model()->findByPk($_GET['id']);
+                    $this->render('detail',array('model'=>$model));
+                }else{
+                $criteria = new CDbCriteria();
+		$criteria->select = '*';
+		$criteria->condition = 'status = 1';                
+                $criteria->order = 'sort_order';
+
+		
+		$model = Alumni::model()->findAll($criteria);
+                
+                $this->render('index',array('model'=>$model));
+                }
+	}
+
+	// Uncomment the following methods and override them if needed
+	/*
+	public function filters()
+	{
+		// return the filter configuration for this controller, e.g.:
+		return array(
+			'inlineFilterName',
+			array(
+				'class'=>'path.to.FilterClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+
+	public function actions()
+	{
+		// return external action classes, e.g.:
+		return array(
+			'action1'=>'path.to.ActionClass',
+			'action2'=>array(
+				'class'=>'path.to.AnotherActionClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+	*/
+}
