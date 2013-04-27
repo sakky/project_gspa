d373e2d111bea0bdd846be954a8973f30d2c3b52
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-27 22:41:47 +0700

    Add Pagination

--- protected/models/Gallery.php
+++ protected/models/Gallery.php
@@ -109,6 +109,7 @@ class Gallery extends CActiveRecord
 
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
+                        'pagination'=>array('pageSize'=> 20),
 		));
 	}
 
