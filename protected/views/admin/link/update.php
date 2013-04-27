950d65f22a80d5e3ca36db78e91328d5e4331e6d
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-28 00:53:21 +0700

    Change text

--- protected/views/admin/link/update.php
+++ protected/views/admin/link/update.php
@@ -3,19 +3,18 @@
 /* @var $model Link */
 
 $this->breadcrumbs=array(
-	'Links'=>array('index'),
-	$model->link_id=>array('view','id'=>$model->link_id),
-	'Update',
+	'Link ที่เกี่ยวข้อง'=>array('index'),
+	'แก้ไขข้อมูล',
 );
 
 $this->menu=array(
 	//array('label'=>'List Link', 'url'=>array('index')),
-	array('label'=>'Create Link', 'url'=>array('create')),
+	array('label'=>'เพิ่ม Link ที่เกี่ยวข้อง', 'url'=>array('create')),
 	//array('label'=>'View Link', 'url'=>array('view', 'id'=>$model->link_id)),
-	array('label'=>'Manage Link', 'url'=>array('admin')),
+	array('label'=>'จัดการ Link ที่เกี่ยวข้อง', 'url'=>array('admin')),
 );
 ?>
 
-<h1>Update Link <?php echo $model->link_id; ?></h1>
+<h1>แก้ไขข้อมูล Link ที่เกี่ยวข้อง #<?php echo $model->link_id; ?></h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
