94fa8ec69badeb9977fd9ef058f1ff6a869f3949
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-27 20:02:20 +0700

    Change Style & Change Link

--- protected/views/web/site/leftmenu.php
+++ protected/views/web/site/leftmenu.php
@@ -1,12 +1,12 @@
 <ul class="list-1">
-        <li><a href="<?php echo Yii::app()->createUrl('site/program'); ?>">หลักสูตรที่เปิดสอน</a></li>
-        <li><a href="#">เอกสารประกอบการเรียนการสอน</a></li>
-        <li><a href="<?php echo Yii::app()->createUrl('page', array('id'=>16)); ?>">วิทยานิพนธ์ / ดุษฎีนิพนธ์</a></li>
-        <li><a href="<?php echo Yii::app()->createUrl('page', array('id'=>17)); ?>">ปัญหาพิเศษ / งานนิพนธ์</a></li>
-        <li><a href="#">จดหมายข่าว Online</a></li>
-        <li><a href="#">รวมพลคนเก่ง GSPA</a></li>
-        <li><a href="#">ข่าวประชาสัมพันธ์นิสิต</a></li>
-        <li><a href="#">ข่าวรับสมัครงาน</a></li>
-        <li><a href="<?php echo Yii::app()->createUrl('page', array('id'=>15)); ?>">สมัครเรียนปริญญาเอก</a></li>          
-        <li class="last-item"><a href="<?php echo Yii::app()->createUrl('page', array('id'=>14)); ?>">สมัครเรียนปริญญาโท</a></li>           
+        <li><h6><a href="<?php echo Yii::app()->createUrl('program'); ?>">หลักสูตรที่เปิดสอน</a></h6></li>
+        <li><h6><a href="<?php echo Yii::app()->createUrl('document'); ?>">เอกสารประกอบการเรียนการสอน</a></h6></li>
+        <li><h6><a href="<?php echo Yii::app()->createUrl('page', array('id'=>16)); ?>">วิทยานิพนธ์ / ดุษฎีนิพนธ์</a></h6></li>
+        <li><h6><a href="<?php echo Yii::app()->createUrl('page', array('id'=>17)); ?>">ปัญหาพิเศษ / งานนิพนธ์</a></h6></li>
+        <li><h6><a href="<?php echo Yii::app()->createUrl('alumni'); ?>">รวมพลคนเก่ง GSPA</a></h6></li>
+        <li><h6><a href="<?php echo Yii::app()->createUrl('news'); ?>">ข่าวประชาสัมพันธ์</a></h6></li>
+        <li><h6><a href="<?php echo Yii::app()->createUrl('studentnews'); ?>">ข่าวประชาสัมพันธ์นิสิต</a></h6></li>
+        <li><h6><a href="<?php echo Yii::app()->createUrl('jobsnews'); ?>">ข่าวรับสมัครงาน</a></h6></li>
+        <li><h6><a href="<?php echo Yii::app()->createUrl('page', array('id'=>15)); ?>">สมัครเรียนปริญญาเอก</a></h6></li>          
+        <li class="last-item"><h6><a href="<?php echo Yii::app()->createUrl('page', array('id'=>14)); ?>">สมัครเรียนปริญญาโท</a></h6></li>           
 </ul>
