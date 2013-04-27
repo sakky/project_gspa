662290c517b957c72f8650c0e8b61b5a0532ef5c
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-28 01:31:51 +0700

    Change Text

--- protected/views/admin/slide/update.php
+++ protected/views/admin/slide/update.php
@@ -3,19 +3,18 @@
 /* @var $model Slide */
 
 $this->breadcrumbs=array(
-	'Slides'=>array('index'),
-	$model->slide_id=>array('view','id'=>$model->slide_id),
-	'Update',
+	'Slide'=>array('index'),
+	'แก้ไขข้อมูล',
 );
 
 $this->menu=array(
 	//array('label'=>'List Slide', 'url'=>array('index')),
-	array('label'=>'Create Slide', 'url'=>array('create')),
+	array('label'=>'เพิ่ม Slide', 'url'=>array('create')),
 	//array('label'=>'View Slide', 'url'=>array('view', 'id'=>$model->slide_id)),
-	array('label'=>'Manage Slide', 'url'=>array('admin')),
+	array('label'=>'จัดการ Slide', 'url'=>array('admin')),
 );
 ?>
 
-<h1>Update Slide <?php echo $model->slide_id; ?></h1>
+<h1>แก้ไข Slide #<?php echo $model->slide_id; ?></h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model,'order_list'=>$order_list)); ?>
