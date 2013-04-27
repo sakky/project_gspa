2e1ffe34ed9c4992968c57eb6d268468ffb4d073
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-27 22:36:57 +0700

    Edit relations in models

--- protected/models/Gallery.php
+++ protected/models/Gallery.php
@@ -62,6 +62,7 @@ class Gallery extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
+			'user' => array(self::BELONGS_TO, 'User', 'user_id'),
 		);
 	}
 
