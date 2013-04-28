44518b26ba469fd8eae68ae34bc49db43a884ff4
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-28 15:43:17 +0700

    Edit Text

--- protected/controllers/admin/DocumentController.php
+++ protected/controllers/admin/DocumentController.php
@@ -1,6 +1,6 @@
 <?php
 
-class DocumentController extends ADminController
+class DocumentController extends AdminController
 {
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
