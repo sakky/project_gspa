e13c72b3b54a872fdd994f1f277f80fe2196b061
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-28 23:11:33 +0700

    change layout

--- protected/views/web/site/leftmenu.php
+++ protected/views/web/site/leftmenu.php
@@ -3,10 +3,6 @@
         <li><h6><a href="<?php echo Yii::app()->createUrl('document'); ?>">เอกสารประกอบการเรียนการสอน</a></h6></li>
         <li><h6><a href="<?php echo Yii::app()->createUrl('page', array('id'=>16)); ?>">วิทยานิพนธ์ / ดุษฎีนิพนธ์</a></h6></li>
         <li><h6><a href="<?php echo Yii::app()->createUrl('page', array('id'=>17)); ?>">ปัญหาพิเศษ / งานนิพนธ์</a></h6></li>
-        <li><h6><a href="<?php echo Yii::app()->createUrl('alumni'); ?>">รวมพลคนเก่ง GSPA</a></h6></li>
-        <li><h6><a href="<?php echo Yii::app()->createUrl('news/news'); ?>">ข่าวประชาสัมพันธ์</a></h6></li>
-        <li><h6><a href="<?php echo Yii::app()->createUrl('news/student'); ?>">ข่าวประชาสัมพันธ์นิสิต</a></h6></li>
-        <li><h6><a href="<?php echo Yii::app()->createUrl('news/job'); ?>">ข่าวรับสมัครงาน</a></h6></li>
         <li><h6><a href="<?php echo Yii::app()->createUrl('page', array('id'=>15)); ?>">สมัครเรียนปริญญาเอก</a></h6></li>          
         <li class="last-item"><h6><a href="<?php echo Yii::app()->createUrl('page', array('id'=>14)); ?>">สมัครเรียนปริญญาโท</a></h6></li>           
 </ul>
