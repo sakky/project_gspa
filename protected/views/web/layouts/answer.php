6b777c3011ee089873239703b6a40952dfa6c679
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-28 15:08:08 +0700

    Remove file not use

--- protected/views/web/layouts/answer.php
+++
@@ -1,6 +0,0 @@
-<?php /* @var $this Controller */ ?>
-<?php $this->beginContent('//layouts/main3'); ?>
-<div id="content">
-	<?php echo $content; ?>
-</div><!-- content -->
-<?php $this->endContent(); ?>
