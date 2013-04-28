44f05326b4578740d97b7e47cf64fe3f9593f8b2
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-28 16:08:09 +0700

    Add Link

--- protected/views/web/news/index.php
+++ protected/views/web/news/index.php
@@ -18,13 +18,13 @@ $this->pageTitle=Yii::app()->name. ' - ข่าวสาร';
           <?php foreach ($news as $new){?>
           <div class="wrapper margin-bot">
           <div class="prev-indent-bot" style="float:left">
-            <figure class="img-border"><a href="#"><img src="<?php echo Yii::app()->request->baseUrl; ?>/uploads/news/<?php echo $new->image;?>" alt="" /></a></figure>
+            <figure class="img-border"><a href="<?php echo Yii::app()->createUrl('news', array('id'=>$new->news_id)); ?>"><img src="<?php echo Yii::app()->request->baseUrl; ?>/uploads/news/<?php echo $new->image;?>" alt="" width="290px" height="157px" /></a></figure>
             <div class="clear"></div>
           </div>
           <div style="float:left;width:300px;margin-left:10px;">
           <h6><?php echo $new->name_th;?></h6>
           <p class="p2"><?php echo $new->title_th;?></p>
-          <a class="button" href="<?php echo Yii::app()->createUrl('news', array('id'=>$new->news_id)); ?>">Read More</a> 
+          <a class="button" href="<?php echo Yii::app()->createUrl('news', array('id'=>$new->news_id)); ?>">อ่านต่อ...</a> 
           </div>
           <div class="clear"></div>
           </div>
