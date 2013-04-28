c993fb186fe96336ce1a3962074cbf00f85d5c76
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-29 00:51:02 +0700

    change layout

--- protected/views/web/document/leftmenu.php
+++ protected/views/web/document/leftmenu.php
@@ -3,3 +3,5 @@
      <li><h6><a href="<?php echo Yii::app()->createUrl('page', array('id'=>16)); ?>">วิทยานิพนธ์ / ดุษฎีนิพนธ์</a></h6></li>
      <li class="last-item"><h6><a href="<?php echo Yii::app()->createUrl('page', array('id'=>17)); ?>">ปัญหาพิเศษ / งานนิพนธ์</a></h6></li>         
 </ul>
+<br/>
+<br/>
