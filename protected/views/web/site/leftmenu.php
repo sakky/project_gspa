8850225c5552159fdb4f64a498b2b9e69985d074
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-29 00:19:19 +0700

    change layout

--- protected/views/web/site/leftmenu.php
+++ protected/views/web/site/leftmenu.php
@@ -3,6 +3,7 @@
         <li><h6><a href="<?php echo Yii::app()->createUrl('document'); ?>">เอกสารประกอบการเรียนการสอน</a></h6></li>
         <li><h6><a href="<?php echo Yii::app()->createUrl('page', array('id'=>16)); ?>">วิทยานิพนธ์ / ดุษฎีนิพนธ์</a></h6></li>
         <li><h6><a href="<?php echo Yii::app()->createUrl('page', array('id'=>17)); ?>">ปัญหาพิเศษ / งานนิพนธ์</a></h6></li>
+
         <li><h6><a href="<?php echo Yii::app()->createUrl('page', array('id'=>15)); ?>">สมัครเรียนปริญญาเอก</a></h6></li>          
         <li class="last-item"><h6><a href="<?php echo Yii::app()->createUrl('page', array('id'=>14)); ?>">สมัครเรียนปริญญาโท</a></h6></li>           
 </ul>
