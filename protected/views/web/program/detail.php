807e4aa3bbbab3185986aeda2cae397e0daac45e
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-04-28 23:11:19 +0700

    change layout

--- protected/views/web/program/detail.php
+++ protected/views/web/program/detail.php
@@ -11,7 +11,6 @@ $this->breadcrumbs=array(
 );
 ?>
 <div id="page6">
-<section id="content" style="margin-top: -20px">
   <div class="main">
     <div class="wrapper">
       <article class="col-1">
@@ -29,5 +28,4 @@ $this->breadcrumbs=array(
       
     </div>
   </div>
-</section>
 </div>
