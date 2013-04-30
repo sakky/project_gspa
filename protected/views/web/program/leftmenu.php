3d589bdbcc997fbd12f3b11b51501f06732f5cd3
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-05-01 01:01:58 +0700

    Change link

--- protected/views/web/program/leftmenu.php
+++ protected/views/web/program/leftmenu.php
@@ -1,5 +1,5 @@
 <ul class="list-1">
         <li><h6><a href="<?php echo Yii::app()->createUrl('program'); ?>">หลักสูตรที่เปิดสอน</a></h6></li>
-        <li><h6><a href="<?php echo Yii::app()->createUrl('page', array('id'=>15)); ?>">สมัครเรียนปริญญาเอก</a></h6></li>          
-        <li class="last-item"><h6><a href="<?php echo Yii::app()->createUrl('page', array('id'=>14)); ?>">สมัครเรียนปริญญาโท</a></h6></li>           
+        <li><h6><a href="<?php echo Yii::app()->createUrl('program/admission', array('id'=>15)); ?>">สมัครเรียนปริญญาเอก</a></h6></li>          
+        <li class="last-item"><h6><a href="<?php echo Yii::app()->createUrl('program/admission', array('id'=>14)); ?>">สมัครเรียนปริญญาโท</a></h6></li>           
 </ul>
