0562a3dcd85b9a69697b269b312aa4c7b6631e0b
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-05-01 12:55:39 +0700

    Add News Type

--- protected/views/web/news/leftmenu.php
+++ protected/views/web/news/leftmenu.php
@@ -1,5 +1,6 @@
 <ul class="list-1">
-       <li><h6><a href="<?php echo Yii::app()->createUrl('news/news'); ?>">ข่าวประชาสัมพันธ์</a></h6></li>
+        <li><h6><a href="<?php echo Yii::app()->createUrl('news/news'); ?>">ข่าวประชาสัมพันธ์</a></h6></li>
         <li><h6><a href="<?php echo Yii::app()->createUrl('news/student'); ?>">ข่าวประชาสัมพันธ์นิสิต</a></h6></li>
-        <li class="last-item"><h6><a href="<?php echo Yii::app()->createUrl('news/job'); ?>">ข่าวรับสมัครงาน</a></h6></li>
+        <li><h6><a href="<?php echo Yii::app()->createUrl('news/job'); ?>">ข่าวรับสมัครงาน</a></h6></li>
+        <li class="last-item"><h6><a href="<?php echo Yii::app()->createUrl('news/advertise'); ?>">ข่าวประกาศ</a></h6></li>
 </ul>
