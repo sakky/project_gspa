a9231c5ee686a8678eb450156fa53e97ab59c630
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-05-05 23:45:55 +0700

    Change name

--- protected/config/_web.php
+++ protected/config/_web.php
@@ -3,8 +3,10 @@
 return CMap::mergeArray(
     require(dirname(__FILE__).'/main.php'),
     array(
-		'name'=>'GSPA : วิทยาลัยการบริหารรัฐกิจ มหาวิทยาลัยบูรพา',
-    )
+		'name'=>'วิทยาลัยการบริหารรัฐกิจ มหาวิทยาลัยบูรพา',
+                'homeUrl'=>array('/site'),
+        )
 );
+
 
 ?>
