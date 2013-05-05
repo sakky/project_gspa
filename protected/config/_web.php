a4141e46bd1dce769b17edeb1e0aa54d496ece32
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-05-06 00:26:34 +0700

    Add 2 Language

--- protected/config/_web.php
+++ protected/config/_web.php
@@ -5,6 +5,39 @@ return CMap::mergeArray(
     array(
 		'name'=>'วิทยาลัยการบริหารรัฐกิจ มหาวิทยาลัยบูรพา',
                 'homeUrl'=>array('/site'),
+                    	'components'=>array(
+			#...
+                        # More setting http://www.yiiframework.com/doc/guide/1.1/en/topics.url
+                        'urlManager'=>array(
+                            'class'=>'ext.yii-multilanguage.MLUrlManager',
+                            'urlFormat'=>'path',
+                            'languages'=>array(
+                                #...
+                                'th',
+                                'en',
+
+                                #...
+                            ),
+                            'rules'=>array(
+                                # ... more user rules
+
+                                '<controller:\w+>/<id:\d+>'=>'<controller>/view',
+                                '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+                                '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+
+                                '<module:\w+>/<controller:\w+>/<id:\d+>/<action:\w+>'=>'<module>/<controller>/<action>',
+                                '<module:\w+>/<controller:\w+>/<id:\d+>'=>'<module>/<controller>/view',
+                                '<module:\w+>/<controller:\w+>/<action:\w+>'=>'<module>/<controller>/<action>',
+                                '<module:\w+>/<controller:\w+>'=>'<module>/<controller>',
+                                '<module:\w+>'=>'<module>',
+
+                                # ...
+                            ),
+                        ),
+                        #...
+			
+
+		),
         )
 );
 
