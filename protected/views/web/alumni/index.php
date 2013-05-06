26f98277ad979a57498cc4cd99cc81a7d9c9dc44
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-05-06 16:16:00 +0700

    Add 2 Language

--- protected/views/web/alumni/index.php
+++ protected/views/web/alumni/index.php
@@ -1,14 +1,31 @@
 <?php
-if((isset($_GET['lang']))&&($_GET['lang'] =='en')){
-    $this->pageTitle=Yii::app()->name . ' - Alumni';
+$lang = Yii::app()->language; 
+if($lang == 'en' || $lang == 'EN'|| $lang == 'En'){
+    $this->pageTitle='Graduate School of Public Administration - Alumni';
+    $this->breadcrumbs=array(
+        'About Us'=>array('&id=1'),
+        'Alumni'
+
+    );
+    $header = "GSPA Alumni";
+    $position_text = "Position";
+    $major_text = "Major";
+    $campus_text ="Campus";
+    $info_text = "More Detail";
  
 }else{
     $this->pageTitle=Yii::app()->name . ' - ศิษย์เก่าดีเด่น';
-    
-}
-$this->breadcrumbs=array(
+    $this->breadcrumbs=array(
+        'เกี่ยวกับเรา'=>array('&id=1'),
         'ศิษย์เก่าดีเด่น'
-);
+
+    );
+    $header = "ศิษย์เก่าดีเด่น";
+    $position_text = "ตำแหน่ง";
+    $major_text = "สาขาวิชา";
+    $campus_text = "ศูนย์การศึกษา";
+    $info_text = "ข้อมูลเพิ่มเติม";
+}
 
 ?>
 <div id="page6">
@@ -20,16 +37,36 @@ $this->breadcrumbs=array(
         </div>
       </article>
       <article class="col-2">
-          <h3>ศิษย์เก่าดีเด่น</h3>
-          <?php foreach ($model as $value){?>
+          <div style="margin-bottom: 10px;">
+            <?php if(isset($this->breadcrumbs)):?>
+                    <?php $this->widget('zii.widgets.CBreadcrumbs', array(
+                            'links'=>$this->breadcrumbs,
+                    )); ?><!-- breadcrumbs -->
+            <?php endif?>
+          </div>
+          <h3><?php echo $header;?></h3>
+          <?php foreach ($model as $value){
+               if($lang == 'en' || $lang == 'EN'|| $lang == 'En'){
+                    $name = $value->name_en;
+                    $major = $value->major_en;
+                    $campus = $value->campus_en;
+                    $position = $value->position_en;
+                    
+                }else{
+                    $name = $value->name_th;  
+                    $major = $value->major_th;
+                    $campus = $value->campus_th;
+                    $position = $value->position_th;
+                }
+          ?>
           <div class="wrapper indent-bot">
             <figure class="img-indent"><img src="<?php echo Yii::app()->request->baseUrl; ?>/uploads/alumni/<?php echo $value->image;?>" width="100px"/></figure>
             <div class="extra-wrap">
-              <h6><?php echo $value->name_th;?></h6>
-              <p>สาขาวิชา : <?php echo $value->major_th;?><br/>
-              ศูนย์การศึกษา : <?php echo $value->campus_th;?><br/>
-              ตำแหน่ง : <?php echo $value->position_th;?><br/>
-              <span class="small"><a href="<?php echo Yii::app()->createUrl('alumni', array('id'=>$value->alumni_id)); ?>">ดูข้อมูลเพิ่มเติม</a></span></p>
+              <h6><?php echo $name;?></h6>
+              <p><?php echo $major_text;?> : <?php echo $major;?><br/>
+              <?php echo $campus_text;?> : <?php echo $campus;?><br/>
+              <?php echo $position_text;?> : <?php echo $position;?><br/>
+              <span class="small"><a href="<?php echo Yii::app()->createUrl('alumni', array('id'=>$value->alumni_id)); ?>"><?php echo $info_text;?></a></span></p>
              </div>
           </div>
           <?php }?>
