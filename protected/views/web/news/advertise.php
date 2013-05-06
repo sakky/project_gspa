421a6a51aeb364763dd38d4a869d005441de5cea
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-05-06 15:28:04 +0700

    Add 2 Language

--- protected/views/web/news/advertise.php
+++ protected/views/web/news/advertise.php
@@ -1,5 +1,23 @@
 <?php
-/* @var $this SiteController */
+/* @var $this NewsController */
+$lang = Yii::app()->language; 
+if($lang == 'en' || $lang == 'EN'|| $lang == 'En'){
+    $this->pageTitle=Yii::app()->name. ' - Announce';
+    $this->breadcrumbs=array(
+            'News'=>array('index'),
+            'Announce'
+    );
+    $header = "Announce";
+    $readmore = "Read More";
+}else{
+    $this->pageTitle=Yii::app()->name. ' - ข่าวประกาศ';
+    $this->breadcrumbs=array(
+            'ข่าวสาร'=>array('index'),
+            'ข่าวประกาศ'
+    );
+    $header = "ข่าวประกาศ";
+    $readmore = "อ่านต่อ...";
+}
 
 $this->pageTitle=Yii::app()->name. ' - ข่าวประกาศ';
 ?>
@@ -12,23 +30,43 @@ $this->pageTitle=Yii::app()->name. ' - ข่าวประกาศ';
         </div>
       </article>
       <article class="col-2">
-          <h3>ข่าวประกาศ</h3>
-          
-          <?php foreach ($news as $new){?>
+          <div style="margin-bottom: 10px;">
+            <?php if(isset($this->breadcrumbs)):?>
+                    <?php $this->widget('zii.widgets.CBreadcrumbs', array(
+                            'links'=>$this->breadcrumbs,
+                    )); ?><!-- breadcrumbs -->
+            <?php endif?>
+          </div>
+          <h3><?php echo $header;?></h3>          
+          <?php foreach ($news as $new){
+              if($lang == 'en' || $lang == 'EN'|| $lang == 'En'){
+                  $name = $new->name_en;
+                  $title = $new->title_en;
+              }else{
+                  $name = $new->name_th;
+                  $title = $new->title_th;
+              }
+          ?>
           <div class="wrapper margin-bot">
           <div class="prev-indent-bot" style="float:left">
             <figure class="img-border"><a href="<?php echo Yii::app()->createUrl('news', array('id'=>$new->news_id)); ?>"><img <?php if($new->image){?> src="<?php echo Yii::app()->request->baseUrl; ?>/uploads/news/<?php echo $new->image;?>"<?php }else{ ?> src="<?php echo Yii::app()->request->baseUrl; ?>/images/front/no_picture2.png"<?php }?> alt="" width="290px" /></a></figure>
             <div class="clear"></div>
           </div>
           <div style="float:left;width:300px;margin-left:10px;">
-          <h6><?php echo $new->name_th;?></h6>
-          <p class="p2"><?php echo $new->title_th;?></p>
-          <a class="button" href="<?php echo Yii::app()->createUrl('news', array('id'=>$new->news_id)); ?>">อ่านต่อ...</a> 
+          <h6><?php echo $name;?></h6>
+          <p class="p2"><?php echo $title;?></p>
+          <a class="button" href="<?php echo Yii::app()->createUrl('news', array('id'=>$new->news_id)); ?>"><?php echo $readmore;?></a> 
           </div>
           <div class="clear"></div>
           </div>
           <?php }?>
-         
+          <?php $this->widget('CLinkPager', array(
+                    'currentPage'=>$pages->getCurrentPage(),
+                    'pages' => $pages,
+                    'maxButtonCount'=>5,
+                    'htmlOptions'=>array('class'=>'pagenav'),
+                    'header'=> '',
+            )) ?>
       </article>     
     </div>
   </div>
