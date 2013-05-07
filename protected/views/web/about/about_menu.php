0a8cb354120695efee9483179e771fe64c15c150
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-05-08 01:09:02 +0700

    Edit Link in breadcrumbs

--- protected/views/web/about/about_menu.php
+++ protected/views/web/about/about_menu.php
@@ -7,7 +7,7 @@ if($lang == 'en' || $lang == 'EN'|| $lang == 'En'){?>
         <li><h6><a href="<?php echo Yii::app()->createUrl('about/board'); ?>">Board of Directors</a></h6></li>
         <li><h6><a href="<?php echo Yii::app()->createUrl('about/executive'); ?>">Executive of GSPA</a></h6></li>
         <li><h6><a href="<?php echo Yii::app()->createUrl('about', array('id'=>4)); ?>">Organization Structure</a></h6></li>
-        <li><h6><a href="<?php echo Yii::app()->createUrl('about', array('id'=>5)); ?>">Academic Cooperation</a></h6></li>     
+        <li><h6><a href="<?php echo Yii::app()->createUrl('cooperation'); ?>">Academic Cooperation</a></h6></li>     
         <li><h6><a href="<?php echo Yii::app()->createUrl('alumni'); ?>">GSPA Alumni</a></h6></li> 
         <li class="last-item"><h6><a href="<?php echo Yii::app()->createUrl('about', array('id'=>6)); ?>">Map</a></h6></li>           
 </ul>
@@ -18,7 +18,7 @@ if($lang == 'en' || $lang == 'EN'|| $lang == 'En'){?>
         <li><h6><a href="<?php echo Yii::app()->createUrl('about/board'); ?>">คณะกรรมการประจำวิทยาลัย</a></h6></li>
         <li><h6><a href="<?php echo Yii::app()->createUrl('about/executive'); ?>">ผู้บริหารวิทยาลัยการบริหารรัฐกิจ</a></h6></li>
         <li><h6><a href="<?php echo Yii::app()->createUrl('about', array('id'=>4)); ?>">โครงสร้างองค์กร</a></h6></li>
-        <li><h6><a href="<?php echo Yii::app()->createUrl('about', array('id'=>5)); ?>">ความร่วมมือทางวิชาการ</a></h6></li>     
+        <li><h6><a href="<?php echo Yii::app()->createUrl('cooperation'); ?>">ความร่วมมือทางวิชาการ</a></h6></li>     
         <li><h6><a href="<?php echo Yii::app()->createUrl('alumni'); ?>">ทำเนียบศิษย์เก่า</a></h6></li> 
         <li class="last-item"><h6><a href="<?php echo Yii::app()->createUrl('about', array('id'=>6)); ?>">แผนที่วิทยาลัย</a></h6></li>           
 </ul>
