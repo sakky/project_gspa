c2fc00e0eff7d868760b70b3d85bbd644ca63bdd
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-05-19 21:29:59 +0700

    Change number of pagesize

--- protected/controllers/web/NewsController.php
+++ protected/controllers/web/NewsController.php
@@ -9,7 +9,7 @@ class NewsController extends Controller
                     $this->render('detail',array('model'=>$model));
                 }else{
                 $news_criteria = new CDbCriteria();
-                $news_criteria->condition = "status = 1";
+                $news_criteria->condition = "status = 1 AND news_type_id = 1";
                 $news_criteria->order = "create_date desc,news_id desc";
                 
                 
@@ -33,13 +33,13 @@ class NewsController extends Controller
                     $this->render('detail',array('model'=>$model));
                 }else{
                 $news_criteria = new CDbCriteria();
-                $news_criteria->condition = "status = 1 AND news_type_id = 1";
+                $news_criteria->condition = "status = 1 AND news_type_id = 5";
                 $news_criteria->order = "create_date desc,news_id desc";
 
                 $news_total = News::model()->count($news_criteria);
 	
 		$pages = new CPagination($news_total);
-                $pages->setPageSize(6);
+                $pages->setPageSize(20);
                 $pages->applyLimit($news_criteria);
                 
                 $news = News::model()->findAll($news_criteria);
@@ -62,7 +62,7 @@ class NewsController extends Controller
                 $news_total = News::model()->count($news_criteria);
 	
 		$pages = new CPagination($news_total);
-                $pages->setPageSize(6);
+                $pages->setPageSize(20);
                 $pages->applyLimit($news_criteria);
                 
                 $news = News::model()->findAll($news_criteria);
@@ -85,7 +85,7 @@ class NewsController extends Controller
                 $news_total = News::model()->count($news_criteria);
 	
 		$pages = new CPagination($news_total);
-                $pages->setPageSize(6);
+                $pages->setPageSize(20);
                 $pages->applyLimit($news_criteria);
                 
                 $news = News::model()->findAll($news_criteria);
@@ -108,7 +108,7 @@ class NewsController extends Controller
                 $news_total = News::model()->count($news_criteria);
 	
 		$pages = new CPagination($news_total);
-                $pages->setPageSize(6);
+                $pages->setPageSize(20);
                 $pages->applyLimit($news_criteria);
                 
                 $news = News::model()->findAll($news_criteria);
