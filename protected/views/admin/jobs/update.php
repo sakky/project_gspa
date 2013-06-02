ef7eeffb9c195621fe8a4271b171d93692a44804
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-06-02 14:29:36 +0700

    edit text

--- protected/views/admin/jobs/update.php
+++ protected/views/admin/jobs/update.php
@@ -3,19 +3,19 @@
 /* @var $model News */
 
 $this->breadcrumbs=array(
-	'News'=>array('index'),
-	$model->news_id=>array('view','id'=>$model->news_id),
-	'Update',
+	'ประกาศ',
+	'รับสมัครงาน'=>array('index'),
+	'แก้ไขข้อมูล',
 );
 
 $this->menu=array(
-	array('label'=>'List News', 'url'=>array('index')),
-	array('label'=>'Create News', 'url'=>array('create')),
-	array('label'=>'View News', 'url'=>array('view', 'id'=>$model->news_id)),
-	array('label'=>'Manage News', 'url'=>array('admin')),
+	//array('label'=>'List News', 'url'=>array('index')),
+	array('label'=>'เพิ่มข้อมูล', 'url'=>array('create')),
+	//array('label'=>'View News', 'url'=>array('view', 'id'=>$model->news_id)),
+	array('label'=>'จัดการข้อมูล', 'url'=>array('admin')),
 );
 ?>
 
-<h1>Update News <?php echo $model->news_id; ?></h1>
+<h1>แก้ไขข้อมูล #<?php echo $model->news_id; ?></h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
