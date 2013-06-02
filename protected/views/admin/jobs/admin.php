c4c761a9e478caadc6a4272c1d13a68d8f8045e2
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-06-02 14:29:58 +0700

    edit text

--- protected/views/admin/jobs/admin.php
+++ protected/views/admin/jobs/admin.php
@@ -3,13 +3,14 @@
 /* @var $model News */
 
 $this->breadcrumbs=array(
-	'News'=>array('index'),
-	'Manage',
+        'ประกาศ',
+	'รับสมัครงาน'=>array('index'),
+	'จัดการข้อมูล',
 );
 
 $this->menu=array(
-	array('label'=>'List News', 'url'=>array('index')),
-	array('label'=>'Create News', 'url'=>array('create')),
+	//array('label'=>'List News', 'url'=>array('index')),
+	array('label'=>'เพิ่มข้อมูล', 'url'=>array('create')),
 );
 
 Yii::app()->clientScript->registerScript('search', "
@@ -26,31 +27,43 @@ $('.search-form form').submit(function(){
 ");
 ?>
 
-<h1>Manage News</h1>
+<h1>จัดการประกาศรับสมัครงาน</h1>
 
-<p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
-
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<?php echo CHtml::link('ค้นหาแบบละเอียด','#',array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
 <?php $this->renderPartial('_search',array(
 	'model'=>$model,
 )); ?>
 </div><!-- search-form -->
 
-<?php $this->widget('zii.widgets.grid.CGridView', array(
+<?php 
+
+$this->widget('zii.widgets.grid.CGridView', array(
 	'id'=>'news-grid',
-	'dataProvider'=>$model->search(),
+	'dataProvider'=>$model->searchJobs(),
 	'filter'=>$model,
 	'columns'=>array(
-		'news_id',
-		'news_type_id',
-		'name_en',
-		'name_th',
-		'title_en',
-		'title_th',
+                array(
+                        'header'=> 'ลำดับ',
+                        'type' => 'raw',
+                        'value' => '$this->grid->dataProvider->pagination->currentPage*$this->grid->dataProvider->pagination->pageSize + $row+1', //this for the auto page number of cgridview
+                        'htmlOptions'=>array('style'=>'text-align: center;width: 30px;'),
+                ),
+		array(
+			'name'=>'name_th',
+                        'header'=> 'ชื่อเรื่องที่ประกาศ',
+			'htmlOptions'=>array('style'=>'text-align: left;'),
+		),
+                array(
+			'name'=>'create_date',
+                        'value'=> 'date(\'d/m/Y\',strtotime($data->create_date))',
+			'htmlOptions'=>array('style'=>'text-align: left;width: 70px;'),
+		),
+                array(
+			'name'=>'status',                 
+			'value'=> '($data->status)? \'แสดง\' : \'ไม่แสดง\'',
+			'htmlOptions'=>array('style'=>'text-align: center;width: 50px;'),
+		),
 		/*
 		'desc_en',
 		'desc_th',
@@ -64,6 +77,9 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		*/
 		array(
 			'class'=>'CButtonColumn',
+                        'template'=>'{update}&nbsp;&nbsp;{delete}',
+                        'headerHtmlOptions'=>array('style'=>'width:40px;'),
+                        'htmlOptions' => array('style'=>'width:40px; text-align:center'),
 		),
 	),
 )); ?>
