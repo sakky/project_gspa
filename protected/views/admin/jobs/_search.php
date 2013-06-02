6bb0223f2f4f36566a23b8c5fb00388d2c9865b5
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-06-02 14:27:45 +0700

    edit text

--- protected/views/admin/jobs/_search.php
+++ protected/views/admin/jobs/_search.php
@@ -17,11 +17,6 @@
 	</div>
 
 	<div class="row">
-		<?php echo $form->label($model,'news_type_id'); ?>
-		<?php echo $form->textField($model,'news_type_id'); ?>
-	</div>
-
-	<div class="row">
 		<?php echo $form->label($model,'name_en'); ?>
 		<?php echo $form->textField($model,'name_en',array('size'=>60,'maxlength'=>255)); ?>
 	</div>
@@ -42,49 +37,37 @@
 	</div>
 
 	<div class="row">
-		<?php echo $form->label($model,'desc_en'); ?>
-		<?php echo $form->textArea($model,'desc_en',array('rows'=>6, 'cols'=>50)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'desc_th'); ?>
-		<?php echo $form->textArea($model,'desc_th',array('rows'=>6, 'cols'=>50)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'image'); ?>
-		<?php echo $form->textField($model,'image',array('size'=>60,'maxlength'=>255)); ?>
-	</div>
-
-	<div class="row">
 		<?php echo $form->label($model,'create_date'); ?>
-		<?php echo $form->textField($model,'create_date'); ?>
-	</div>
+		<?php list($year,$month,$day) = explode('-',$model->create_date);
+                      $crate_date = $day.'/'.$month.'/'.$year;
+                      $this->widget('zii.widgets.jui.CJuiDatePicker', array(
+                        'model' => $model,
+                        'attribute' => 'create_date',
+                        'language' => 'th',
 
-	<div class="row">
-		<?php echo $form->label($model,'show_homepage'); ?>
-		<?php echo $form->textField($model,'show_homepage'); ?>
-	</div>
+                        'options'=>array(
+                                    'showAnim'=>'fold',
+                                    'dateFormat'=>'dd/mm/yy',
+                                    'changeMonth'=>true,
+                                    'changeYear'=>true,
+                                    'changeDate'=>true,
+                                    'showAnim'=>'fold',
+                                    //'showButtonPanel'=>true,
+                                    'debug'=>true,
 
-	<div class="row">
-		<?php echo $form->label($model,'show_new'); ?>
-		<?php echo $form->textField($model,'show_new'); ?>
+                                    ),
+                        'htmlOptions' => array(
+                            'class'=>'shadowdatepicker',
+                            'readonly'=>"readonly",
+                        ),
+                    )) ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->label($model,'status'); ?>
-		<?php echo $form->textField($model,'status'); ?>
+		<?php echo $form->dropDownList($model, 'status', array('1'=>'แสดง','0'=>'ไม่แสดง')); ?>		
 	</div>
 
-	<div class="row">
-		<?php echo $form->label($model,'user_id'); ?>
-		<?php echo $form->textField($model,'user_id'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'time_stamp'); ?>
-		<?php echo $form->textField($model,'time_stamp'); ?>
-	</div>
 
 	<div class="row buttons">
 		<?php echo CHtml::submitButton('Search'); ?>
