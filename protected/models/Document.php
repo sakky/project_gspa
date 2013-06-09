f75e815016c3496254eeb4f5eefedd86fdd301be
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-06-09 15:03:33 +0700

    Edit test & Add field

--- protected/models/Document.php
+++ protected/models/Document.php
@@ -47,14 +47,14 @@ class Document extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('name_en, name_th', 'required','message'=>'{attribute} ห้ามว่าง'),
-			array('counter, sort_order, status, user_id', 'numerical', 'integerOnly'=>true),
+			array('name_en, name_th,doc_type_id', 'required','message'=>'{attribute} ห้ามว่าง'),
+			array('counter, sort_order, status, user_id,doc_type_id', 'numerical', 'integerOnly'=>true),
 			array('name_en, name_th', 'length', 'max'=>255),
-			array('last_update', 'safe'),
+			array('last_update, desc_en, desc_th', 'safe'),
                         array('pdf_en,pdf_th', 'file', 'types'=>'pdf,zip', 'maxSize'=>1024 * 1024 * 15, 'tooLarge'=>'File has to be smaller than 15MB','allowEmpty'=>true) ,
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('doc_id, name_en, name_th, pdf_en, pdf_th, last_update, counter, sort_order, status, user_id, time_stamp', 'safe', 'on'=>'search'),
+			array('doc_id,doc_type_id, name_en, name_th, desc_en, desc_th, pdf_en, pdf_th, last_update, counter, sort_order, status, user_id, time_stamp', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -67,6 +67,7 @@ class Document extends CActiveRecord
 		// class name for the relations automatically generated below.
 		return array(
 			'user' => array(self::BELONGS_TO, 'User', 'user_id'),
+                        'documentType' => array(self::BELONGS_TO, 'DocumentType', 'doc_type_id'),
 		);
 	}
 
@@ -77,8 +78,11 @@ class Document extends CActiveRecord
 	{
 		return array(
 			'doc_id' => 'รหัส',
-			'name_en' => 'ชื่อเอกสาร (ภาษาอังกฤษ)',
-			'name_th' => 'ชื่อเอกสาร (ภาษาไทย)',
+			'name_en' => 'ชื่อเรื่อง (ภาษาอังกฤษ)',
+			'name_th' => 'ชื่อเรื่อง (ภาษาไทย)',
+                        'doc_type_id' => 'ประเภทสื่อเผยแพร่',
+                    	'desc_en' => 'รายละเอียด (ภาษาอังกฤษ)',
+			'desc_th' => 'รายละเอียด (ภาษาไทย)',
 			'pdf_en' => 'อัพโหลดไฟล์ (ภาษาอังกฤษ)',
 			'pdf_th' => 'อัพโหลดไฟล์ (ภาษาไทย)',
 			'last_update' => 'วันที่ปรับปรุงข้อมูล',
@@ -102,8 +106,11 @@ class Document extends CActiveRecord
 		$criteria=new CDbCriteria;
 
 		$criteria->compare('doc_id',$this->doc_id);
+                $criteria->compare('doc_type_id',$this->doc_type_id);
 		$criteria->compare('name_en',$this->name_en,true);
 		$criteria->compare('name_th',$this->name_th,true);
+                $criteria->compare('desc_en',$this->desc_en,true);
+		$criteria->compare('desc_th',$this->desc_th,true);
 		$criteria->compare('pdf_en',$this->pdf_en,true);
 		$criteria->compare('pdf_th',$this->pdf_th,true);
 		$criteria->compare('last_update',$this->last_update,true);
