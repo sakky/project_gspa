f5c8a35e255dfea915ce59993029af335a509fc4
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-06-09 15:02:58 +0700

    Edit test & Add field

--- protected/views/admin/document/create.php
+++ protected/views/admin/document/create.php
@@ -3,16 +3,16 @@
 /* @var $model Document */
 
 $this->breadcrumbs=array(
-	'เอกสารประกอบการเรียน'=>array('index'),
+	'สื่อเผยแพร่/ดาวน์โหลด'=>array('index'),
 	'เพิ่มข้อมูล',
 );
 
 $this->menu=array(
 	//array('label'=>'List Document', 'url'=>array('index')),
-	array('label'=>'จัดการข้อมูลเอกสาร', 'url'=>array('admin')),
+	array('label'=>'จัดการข้อมูลสื่อเผยแพร่', 'url'=>array('admin')),
 );
 ?>
 
-<h1>เพิ่มข้อมูล เอกสารประกอบการเรียน</h1>
+<h1>เพิ่มข้อมูล สื่อเผยแพร่/ดาวน์โหลด</h1>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+<?php echo $this->renderPartial('_form', array('model'=>$model,'doc_type_list'=>$doc_type_list)); ?>
