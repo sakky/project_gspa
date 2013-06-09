8572f683fe897c828590b9d9c9651e97b2ba0eea
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-06-09 15:03:11 +0700

    Edit test & Add field

--- protected/views/admin/document/update.php
+++ protected/views/admin/document/update.php
@@ -3,18 +3,18 @@
 /* @var $model Document */
 
 $this->breadcrumbs=array(
-	'เอกสารประกอบการเรียน'=>array('index'),
+	'สื่อเผยแพร่/ดาวน์โหลด'=>array('index'),
 	'แก้ไขข้อมูล',
 );
 
 $this->menu=array(
 	//array('label'=>'List Document', 'url'=>array('index')),
-	array('label'=>'เพิ่มข้อมูลเอกสาร', 'url'=>array('create')),
+	array('label'=>'เพิ่มข้อมูลสื่อเผยแพร่', 'url'=>array('create')),
 	//array('label'=>'View Document', 'url'=>array('view', 'id'=>$model->doc_id)),
-	array('label'=>'จัดการข้อมูลเอกสาร', 'url'=>array('admin')),
+	array('label'=>'จัดการข้อมูลสื่อเผยแพร่', 'url'=>array('admin')),
 );
 ?>
 
-<h1>แก้ไขข้อมูล เอกสารประกอบการเรียน #<?php echo $model->doc_id; ?></h1>
+<h1>แก้ไขข้อมูล สื่อเผยแพร่/ดาวน์โหลด #<?php echo $model->doc_id; ?></h1>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+<?php echo $this->renderPartial('_form', array('model'=>$model,'doc_type_list'=>$doc_type_list)); ?>
