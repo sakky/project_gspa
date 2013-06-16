eb53966cf2646aac6defec0efec18f5416f5bcb9
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-06-16 21:32:26 +0700

    Set require field new_group_id

--- protected/models/News.php
+++ protected/models/News.php
@@ -48,15 +48,15 @@ class News extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('news_type_id, name_en, name_th,title_en,title_th,create_date, status,', 'required','message'=>'{attribute} ห้ามว่าง'),
-			array('news_type_id, show_homepage, show_new, status, user_id', 'numerical', 'integerOnly'=>true),
+			array('news_type_id, news_group_id, name_en, name_th,title_en,title_th,create_date, status,', 'required','message'=>'{attribute} ห้ามว่าง'),
+			array('news_type_id, news_group_id, show_homepage, show_new, status, user_id', 'numerical', 'integerOnly'=>true),
 			array('name_en, name_th, image', 'length', 'max'=>255),
 			array('title_en, title_th, desc_en, desc_th, create_date', 'safe'),
                         array('image, thumbnail', 'file', 'types'=>'jpg, jpeg, gif, png', 'allowEmpty'=>true),
                         array('pdf_en,pdf_th', 'file', 'types'=>'pdf', 'maxSize'=>1024 * 1024 * 10, 'tooLarge'=>'ไฟล์ควรมีขนาดเล็กกว่า 10 MB','allowEmpty'=>true) ,
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('news_id, news_type_id, name_en, name_th, title_en, title_th, desc_en, desc_th, image, thumbnail, create_date, show_homepage, show_new, status, user_id, time_stamp', 'safe', 'on'=>'search'),
+			array('news_id, news_group_id, news_type_id, name_en, name_th, title_en, title_th, desc_en, desc_th, image, thumbnail, create_date, show_homepage, show_new, status, user_id, time_stamp', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -69,6 +69,7 @@ class News extends CActiveRecord
 		// class name for the relations automatically generated below.
 		return array(
                     'newsType' => array(self::BELONGS_TO, 'NewsType', 'news_type_id', 'joinType'=>'INNER JOIN'),
+                    'newsGroup' => array(self::BELONGS_TO, 'NewsGroup', 'news_group_id', 'joinType'=>'INNER JOIN'),
 		);
 	}
 
@@ -80,6 +81,7 @@ class News extends CActiveRecord
 		return array(
 			'news_id' => 'รหัส',
 			'news_type_id' => 'ประเภทข่าว',
+                        'news_group_id' => 'ประเภท',
 			'name_en' => 'หัวข้อข่าว (ภาษาอังกฤษ)',
 			'name_th' => 'หัวข้อข่าว (ภาษาไทย)',
 			'title_en' => 'คำบรรยาย (ภาษาอังกฤษ)',
@@ -143,6 +145,38 @@ class News extends CActiveRecord
 
 		$criteria->compare('news_id',$this->news_id);
 		$criteria->compare('news_type_id',3);
+		$criteria->compare('name_en',$this->name_en,true);
+		$criteria->compare('name_th',$this->name_th,true);
+		$criteria->compare('title_en',$this->title_en,true);
+		$criteria->compare('title_th',$this->title_th,true);
+		$criteria->compare('desc_en',$this->desc_en,true);
+		$criteria->compare('desc_th',$this->desc_th,true);
+                $criteria->compare('pdf_en',$this->pdf_en,true);
+		$criteria->compare('pdf_th',$this->pdf_th,true);
+		$criteria->compare('image',$this->image,true);
+                $criteria->compare('thumbnail',$this->thumbnail,true);
+		$criteria->compare('create_date',$this->create_date,true);
+		$criteria->compare('show_homepage',$this->show_homepage);
+		$criteria->compare('show_new',$this->show_new);
+		$criteria->compare('status',$this->status);
+		$criteria->compare('user_id',$this->user_id);
+		$criteria->compare('time_stamp',$this->time_stamp,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+                        'pagination'=>array('pageSize'=> 20),
+		));
+	}
+        
+        public function searchInSide()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('news_id',$this->news_id);
+		$criteria->compare('news_type_id',5);
 		$criteria->compare('name_en',$this->name_en,true);
 		$criteria->compare('name_th',$this->name_th,true);
 		$criteria->compare('title_en',$this->title_en,true);
