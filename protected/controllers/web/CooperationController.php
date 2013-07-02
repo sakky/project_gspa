2b2c7b05b091fd3aa659bb6484b5ad388ef8b2db
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-07-03 03:05:02 +0700

    Chage Module Cooperation

--- protected/controllers/web/CooperationController.php
+++ protected/controllers/web/CooperationController.php
@@ -4,18 +4,57 @@ class CooperationController extends Controller
 {
 	public function actionIndex()
 	{
+                if(isset($_GET['type_id'])){
+                    $criteria = new CDbCriteria();
+                    $criteria->select = '*';
+                    $criteria->condition = 'status = 1 AND co_type_id ='.$_GET['type_id'];                
+                    $criteria->order = 'sort_order ASC ,co_id ASC';
+                    $model = Cooperation::model()->findAll($criteria);                              
+                    $this->render('index',array('model'=>$model));
+                }if(isset($_GET['id'])){
+                    $model=Cooperation::model()->findByPk($_GET['id']);
+                    $this->render('detail',array('model'=>$model));
+                }else{
+                    $criteria = new CDbCriteria();
+                    $criteria->select = '*';
+                    $criteria->condition = 'status = 1';                
+                    $criteria->order = 'sort_order ASC ,co_id ASC';		
+                    $model = Cooperation::model()->findAll($criteria);                              
+                    $this->render('index',array('model'=>$model));
+                }
+
+	}
+        
+        public function actionInbound()
+	{
                 if(isset($_GET['id'])){
                     $model=Cooperation::model()->findByPk($_GET['id']);
                     $this->render('detail',array('model'=>$model));
                 }else{
                 $criteria = new CDbCriteria();
 		$criteria->select = '*';
-		$criteria->condition = 'status = 1';                
-                $criteria->order = 'sort_order ASC ,co_id ASC';
-
-		
+                $criteria->condition = 'status=:status AND t.group=:group';
+                $criteria->params=array(':status'=>1,':group'=>'inbound');               
+                $criteria->order = 'sort_order ASC ,co_id ASC';		
 		$model = Cooperation::model()->findAll($criteria);                              
-                $this->render('index',array('model'=>$model));
+                $this->render('inbound',array('model'=>$model));
+                }
+
+	}
+        
+        public function actionOutbound()
+	{
+                if(isset($_GET['id'])){
+                    $model=Cooperation::model()->findByPk($_GET['id']);
+                    $this->render('detail',array('model'=>$model));
+                }else{
+                $criteria = new CDbCriteria();
+		$criteria->select = '*';
+                $criteria->condition = 'status=:status AND t.group=:group';
+                $criteria->params=array(':status'=>1,':group'=>'outbound');               
+                $criteria->order = 'sort_order ASC ,co_id ASC';		
+		$model = Cooperation::model()->findAll($criteria);                              
+                $this->render('outbound',array('model'=>$model));
                 }
 
 	}
