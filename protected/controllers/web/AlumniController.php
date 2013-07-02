dfadb906357bd6a8b992187b3d7d78a9894e208e
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-07-03 03:53:52 +0700

    modify module alumni

--- protected/controllers/web/AlumniController.php
+++ protected/controllers/web/AlumniController.php
@@ -7,16 +7,54 @@ class AlumniController extends Controller
                 if(isset($_GET['id'])){
                     $model=Alumni::model()->findByPk($_GET['id']);
                     $this->render('detail',array('model'=>$model));
+                }else if(isset($_GET['type_id'])){
+                    $criteria = new CDbCriteria();
+                    $criteria->select = '*';
+                    $criteria->condition = 'status = 1 AND alumni_no_id='.$_GET['type_id'];                
+                    $criteria->order = 'sort_order';
+                    $model = Alumni::model()->findAll($criteria);
+                    $this->render('index',array('model'=>$model));
+                }else{
+                    $criteria = new CDbCriteria();
+                    $criteria->select = '*';
+                    $criteria->condition = 'status = 1';                
+                    $criteria->order = 'sort_order';
+                    $model = Alumni::model()->findAll($criteria);
+                    $this->render('index',array('model'=>$model));
+                }
+	}
+        
+        public function actionMaster()
+	{
+                if(isset($_GET['id'])){
+                    $model=Alumni::model()->findByPk($_GET['id']);
+                    $this->render('detail',array('model'=>$model));
                 }else{
                 $criteria = new CDbCriteria();
 		$criteria->select = '*';
-		$criteria->condition = 'status = 1';                
+		$criteria->condition = 'status = 1 AND alumni_group=\'Master\'';                
                 $criteria->order = 'sort_order';
-
 		
 		$model = Alumni::model()->findAll($criteria);
                 
-                $this->render('index',array('model'=>$model));
+                $this->render('master',array('model'=>$model));
+                }
+	}
+        
+        public function actionDoctor()
+	{
+                if(isset($_GET['id'])){
+                    $model=Alumni::model()->findByPk($_GET['id']);
+                    $this->render('detail',array('model'=>$model));
+                }else{
+                $criteria = new CDbCriteria();
+		$criteria->select = '*';
+		$criteria->condition = 'status = 1 AND alumni_group=\'Doctor\'';                
+                $criteria->order = 'sort_order';
+		
+		$model = Alumni::model()->findAll($criteria);
+                
+                $this->render('doctor',array('model'=>$model));
                 }
 	}
 
