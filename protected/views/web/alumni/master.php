d7732510755a4f289cf3eb1d6f5ecc2096ee7495
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-07-03 03:53:30 +0700

    modify module alumni

--- protected/views/web/alumni/master.php
+++ protected/views/web/alumni/master.php
@@ -4,37 +4,29 @@ if($lang == 'en' || $lang == 'EN'|| $lang == 'En'){
     $this->pageTitle='Graduate School of Public Administration - Alumni';
     $this->breadcrumbs=array(
         'Alumni'=>array('index'),
-        $model->name_en
+        'Master Degree'
 
     );
-    $header = "GSPA Alumni";
+    $header = "GSPA Alumni :: Master Degree";
     $position_text = "Position";
     $major_text = "Major";
+    $gen_text = "Graduate";
     $campus_text ="Campus";
     $info_text = "More Detail";
-    $name = $model->name_en;
-    $major = $model->major_en;
-    $campus = $model->campus_en;
-    $position = $model->position_en;
-    $desc = $model->desc_en;
  
 }else{
     $this->pageTitle=Yii::app()->name . ' - ทำเนียบนิสิต';
     $this->breadcrumbs=array(
         'ทำเนียบนิสิต'=>array('index'),
-         $model->name_th
+        'ปริญญาโท'
 
     );
-    $header = "ทำเนียบนิสิต";
+    $header = "ทำเนียบนิสิต :: ปริญญาโท";
     $position_text = "ตำแหน่ง";
     $major_text = "สาขาวิชา";
+    $gen_text = "รุ่นที่จบ";
     $campus_text = "ศูนย์การศึกษา";
     $info_text = "ข้อมูลเพิ่มเติม";
-    $name = $model->name_th;
-    $major = $model->major_th;
-    $campus = $model->campus_th;
-    $position = $model->position_th;
-    $desc = $model->desc_th;
 }
 
 ?>
@@ -42,8 +34,8 @@ if($lang == 'en' || $lang == 'EN'|| $lang == 'En'){
   <div class="main">
     <div class="wrapper">
       <article class="col-1">
-        <div class="indent-left">
-          <?php $this->renderPartial('/alumni/leftmenu');?>
+        <div class="indent-left">            
+            <?php $this->renderPartial('/alumni/leftmenu');?>
         </div>
       </article>
       <article class="col-2">
@@ -55,17 +47,36 @@ if($lang == 'en' || $lang == 'EN'|| $lang == 'En'){
             <?php endif?>
           </div>
           <h3><?php echo $header;?></h3>
+          <?php foreach ($model as $value){
+               if($lang == 'en' || $lang == 'EN'|| $lang == 'En'){
+                    $name = $value->name_en;
+                    $major = $value->major_en;
+                    $campus = $value->campus_en;
+                    $position = $value->position_en;
+                    $gen = $value->alumniType->name_en;
+                    
+                }else{
+                    $name = $value->name_th;  
+                    $major = $value->major_th;
+                    $campus = $value->campus_th;
+                    $position = $value->position_th;
+                    $gen = $value->alumniType->name_th;
+                }
+          ?>
           <div class="wrapper indent-bot">
-            <figure class="img-indent"><img src="<?php echo Yii::app()->request->baseUrl; ?>/uploads/alumni/<?php echo $model->image;?>" width="100px"/></figure>
+            <figure class="img-indent"><img src="<?php echo Yii::app()->request->baseUrl; ?>/uploads/alumni/<?php echo $value->image;?>" width="100px"/></figure>
             <div class="extra-wrap">
               <h6><?php echo $name;?></h6>
               <p><?php echo $major_text;?> : <?php echo $major;?><br/>
+              <?php echo $gen_text;?> : <?php echo $gen;?><br/>
               <?php echo $campus_text;?> : <?php echo $campus;?><br/>
               <?php echo $position_text;?> : <?php echo $position;?><br/>
-              <?php echo $info_text;?> : <br/><?php echo $desc;?></p>
-            </div>
-          </div>          
-      </article>      
+              <span class="small"><a href="<?php echo Yii::app()->createUrl('alumni', array('id'=>$value->alumni_id)); ?>"><?php echo $info_text;?></a></span></p>
+             </div>
+          </div>
+          <?php }?>
+        </article>
+      
     </div>
   </div>
 </div>
