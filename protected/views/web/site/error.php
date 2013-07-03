d8be2731bf15f32aeb2cc8076b57996c3951c408
sakky <sakky@192.168.212.87>
2013-07-03 18:27:38 +0700

    Change layout of 404 error

--- protected/views/web/site/error.php
+++ protected/views/web/site/error.php
@@ -2,14 +2,21 @@
 /* @var $this SiteController */
 /* @var $error array */
 
-$this->pageTitle=Yii::app()->name . ' - Error';
+$this->pageTitle=Yii::app()->name . ' - Page Missing';
 $this->breadcrumbs=array(
-	'Error',
+	'Page Missing',
 );
 ?>
+<div id="content">
+    <div id="page3">
+      <div class="main" style="height:auto;padding: 100px;text-align: center;">       
+            <h2>Error <?php echo $code; ?></h2>
 
-<h2>Error <?php echo $code; ?></h2>
+            <div class="error">
+            <?php echo CHtml::encode($message); ?>
+            </div>
 
-<div class="error">
-<?php echo CHtml::encode($message); ?>
+        </div>
+    </div>
 </div>
+
