91ded2c28aef085b9d52164b4f23b51dab4f7aa8
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-07-06 01:19:02 +0700

    add page faq & privacy policy

---
+++ protected/views/web/site/privacy.php
@@ -0,0 +1,37 @@
+<?php
+if((isset($_GET['lang']))&&($_GET['lang'] =='en')){
+    $this->pageTitle=Yii::app()->name . ' - '.$model->name_en;
+    $header = $model->name_en;
+    $desc = $model->desc_en;
+}else{
+    $this->pageTitle=Yii::app()->name . ' - '.$model->name_th;
+    $header = $model->name_th;
+    $desc = $model->desc_th;
+}
+$this->breadcrumbs=array(
+	$header,
+);
+
+?><!--==============================content================================-->
+<section id="content">
+  <div class="main">
+    
+    <div class="wrapper">
+        <div style="padding: 0 15px;">
+           <div style="margin-bottom: 10px;">
+            <?php if(isset($this->breadcrumbs)):?>
+                    <?php $this->widget('zii.widgets.CBreadcrumbs', array(
+                            'links'=>$this->breadcrumbs,
+                    )); ?><!-- breadcrumbs -->
+            <?php endif?>
+          </div>
+            <h3><?php echo $header;?></h3>
+            <?php echo $desc;?>
+
+           <div class="clear"></div>
+
+         </div>
+
+    </div>
+  </div>
+</section>
