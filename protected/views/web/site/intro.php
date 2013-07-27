f08838b625896d056a78e95a40b4831dd4826a14
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-07-28 01:43:02 +0700

    intro page layout

---
+++ protected/views/web/site/intro.php
@@ -0,0 +1,17 @@
+<!-- Top Banner -->
+<div class="main">
+    <div class="lang">
+    	   <a title="ภาษาไทย" href="<?php echo Yii::app()->UrlManager->createLanguageUrl('th');?>"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/front/flag_th.png" alt="Thai" border="0" /></a>
+           <a title="English" href="<?php echo Yii::app()->UrlManager->createLanguageUrl('en');?>"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/front/flag_en.png" alt="English" border="0" /></a>
+    </div>
+</div>
+<section id="content">
+  <div class="main">
+    <div class="slider-wrapper">
+      
+    </div>
+    <div align="center">
+        <a class="button" href="<?php echo Yii::app()->createUrl('site/home'); ?>">เข้าสู่เว็บไซต์</a>
+    </div>
+  </div>
+</section>
