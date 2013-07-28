9d864a7131aa08a7eddae0f6a12dcec447f624c4
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-07-28 23:55:07 +0700

    edit personnel

--- protected/views/admin/personnel/update.php
+++ protected/views/admin/personnel/update.php
@@ -18,4 +18,4 @@ $this->menu=array(
 
 <h1>Update Personnel <?php echo $model->personnel_id; ?></h1>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+<?php echo $this->renderPartial('_form', array('model'=>$model,'personnel_type_list'=>$personnel_type_list,)); ?>
