19db047940f6db12c79c0d31ec2f138b857f343e
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-07-29 22:56:41 +0700

    edit wrong word

--- protected/views/web/student/leftmenu.php
+++ protected/views/web/student/leftmenu.php
@@ -49,7 +49,7 @@ if($lang == 'en' || $lang == 'EN'|| $lang == 'En'){?>
         ?>
         <li>&nbsp;&nbsp;&nbsp;&nbsp;<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/front/marker_2.gif" border="0" style="padding-top: 7px"/>&nbsp;<a href="<?php echo Yii::app()->createUrl('student', array('type_id'=>$type->ser_type_id)); ?>"><?php echo $type->name_th;?></a></li>        
         <?php }?>
-        <li><h6><a href="<?php echo Yii::app()->createUrl('student/group2'); ?>">ปริญาเอก</a></h6></li>
+        <li><h6><a href="<?php echo Yii::app()->createUrl('student/group2'); ?>">ปริญญาเอก</a></h6></li>
         <?php
             $criteria = new CDbCriteria();
             $criteria->condition = 'status = 1 AND ser_group=2'; 
