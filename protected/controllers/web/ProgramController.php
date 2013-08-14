da73b2395df26d4b0c2d5de4a7157f670b258f19
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-08-15 01:33:33 +0700

    show programs

--- protected/controllers/web/ProgramController.php
+++ protected/controllers/web/ProgramController.php
@@ -27,14 +27,57 @@ class ProgramController extends Controller
                 $this->render('index',array('master'=>$master,'doctor'=>$doctor));
                 }
 	}
-        
-        public function actionAdmission()
+        public function actionMaster()
 	{
+                if(isset($_GET['id'])){
+                    $model=Program::model()->findByPk($_GET['id']);
+                    $this->render('detail',array('model'=>$model));
+                }else{
+                    $criteria = new CDbCriteria();
+                    $criteria->select = '*';
+                    $criteria->condition = 'status = 1 AND program_type=\'Master\'';                
+                    $criteria->order = 'sort_order';
+                    $total = Program::model()->count($criteria); 
+                    
+                    
+                    $pages = new CPagination($total);
+                    $pages->setPageSize(20);
+                    $pages->applyLimit($criteria);                
 		
-                    $model=Page::model()->findByPk($_GET['id']);
-                    $this->render('admission',array('model'=>$model));
-               
+                    $model = Program::model()->findAll($criteria);
+
+                    $this->render('master',array('model'=>$model,'pages'=> $pages));
+                }
 	}
+        
+        public function actionDoctor()
+	{
+                if(isset($_GET['id'])){
+                    $model=Program::model()->findByPk($_GET['id']);
+                    $this->render('detail',array('model'=>$model));
+                }else{
+                    $criteria = new CDbCriteria();
+                    $criteria->select = '*';
+                    $criteria->condition = 'status = 1 AND program_type=\'Doctor\'';                
+                    $criteria->order = 'sort_order';
+                    $total = Program::model()->count($criteria);                    
+                    
+                    $pages = new CPagination($total);
+                    $pages->setPageSize(20);
+                    $pages->applyLimit($criteria);                   
+		
+                    $model = Program::model()->findAll($criteria);
+
+                    $this->render('doctor',array('model'=>$model,'pages'=> $pages));
+                }
+	}
+//        public function actionAdmission()
+//	{
+//		
+//                    $model=Page::model()->findByPk($_GET['id']);
+//                    $this->render('admission',array('model'=>$model));
+//               
+//	}
 
 	// Uncomment the following methods and override them if needed
 	/*
