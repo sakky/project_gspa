603691e7b90c298d16cb5f30de64f084e588c6b0
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-08-18 14:57:07 +0700

    add banner image

--- protected/views/web/announce/admission.php
+++ protected/views/web/announce/admission.php
@@ -35,9 +35,7 @@ if($lang == 'en' || $lang == 'EN'|| $lang == 'En'){
                     )); ?><!-- breadcrumbs -->
             <?php endif?>
           </div>
-
-          <div><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/front/announce.jpg"/></div>
-          
+          <div><img class="border-banner"  src="<?php echo Yii::app()->request->baseUrl; ?>/images/front/admission-banner.jpg"/></div>          
           <h3><?php echo $header;?></h3>
           <ul class="list-2">
           <?php foreach ($news as $new){
