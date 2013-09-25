fb3b97f75cfe6d621359d19cc208264d72bc0f70
Kanokwan <Kanokwan@Kanokwan-PC.mshome.net>
2013-09-26 01:06:27 +0700

    Fix Sidebar

--- protected/views/web/cooperation/leftmenu.php
+++ protected/views/web/cooperation/leftmenu.php
@@ -12,7 +12,7 @@ if($lang == 'en' || $lang == 'EN'|| $lang == 'En'){?>
 
             foreach($co_type as $type) {
         ?>
-        <li>&nbsp;&nbsp;&nbsp;&nbsp;<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/front/marker_2.gif" border="0" style="padding-top: 7px"/>&nbsp;<a href="<?php echo Yii::app()->createUrl('cooperation', array('type_id'=>$type->co_type_id)); ?>"><?php echo $type->name_en;?></a></li>        
+        <li><a href="<?php echo Yii::app()->createUrl('cooperation', array('type_id'=>$type->co_type_id)); ?>"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/front/marker_2.gif" border="0" style="padding-top: 7px"/>&nbsp;<?php echo $type->name_en;?></a></li>        
         <?php }?>
         <li><h6><a href="<?php echo Yii::app()->createUrl('cooperation/outbound'); ?>">International</a></h6></li>
         <?php
@@ -24,7 +24,7 @@ if($lang == 'en' || $lang == 'EN'|| $lang == 'En'){?>
 
             foreach($co_type as $type) {
         ?>
-        <li>&nbsp;&nbsp;&nbsp;&nbsp;<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/front/marker_2.gif" border="0" style="padding-top: 7px"/>&nbsp;<a href="<?php echo Yii::app()->createUrl('cooperation', array('type_id'=>$type->co_type_id)); ?>"><?php echo $type->name_en;?></a></li>        
+        <li><a href="<?php echo Yii::app()->createUrl('cooperation', array('type_id'=>$type->co_type_id)); ?>"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/front/marker_2.gif" border="0" style="padding-top: 7px"/>&nbsp;<?php echo $type->name_en;?></a></li>        
         <?php }?>
 </ul>
 <?php }else{?>
@@ -39,7 +39,7 @@ if($lang == 'en' || $lang == 'EN'|| $lang == 'En'){?>
 
             foreach($co_type as $type) {
         ?>
-        <li>&nbsp;&nbsp;&nbsp;&nbsp;<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/front/marker_2.gif" border="0" style="padding-top: 7px"/>&nbsp;<a href="<?php echo Yii::app()->createUrl('cooperation', array('type_id'=>$type->co_type_id)); ?>"><?php echo $type->name_th;?></a></li>        
+        <li><a href="<?php echo Yii::app()->createUrl('cooperation', array('type_id'=>$type->co_type_id)); ?>"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/front/marker_2.gif" border="0" style="padding-top: 7px"/>&nbsp;<?php echo $type->name_th;?></a></li>        
         <?php }?>
         <li><h6><a href="<?php echo Yii::app()->createUrl('cooperation/outbound'); ?>">ต่างประเทศ</a></h6></li>
         <?php
@@ -51,7 +51,7 @@ if($lang == 'en' || $lang == 'EN'|| $lang == 'En'){?>
 
             foreach($co_type as $type) {
         ?>
-        <li>&nbsp;&nbsp;&nbsp;&nbsp;<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/front/marker_2.gif" border="0" style="padding-top: 7px"/>&nbsp;<a href="<?php echo Yii::app()->createUrl('cooperation', array('type_id'=>$type->co_type_id)); ?>"><?php echo $type->name_th;?></a></li>        
+        <li><a href="<?php echo Yii::app()->createUrl('cooperation', array('type_id'=>$type->co_type_id)); ?>"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/front/marker_2.gif" border="0" style="padding-top: 7px"/>&nbsp;<?php echo $type->name_th;?></a></li>        
         <?php }?>
 </ul>
 <?php } ?>
